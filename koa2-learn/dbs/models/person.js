7691acf2234851f47c11bfd620babe6544d312f9
lijingke <1431806069@qq.com>
2019-03-04 16:00:59 +0800

    koa-router定义接口并对mongoose数据库增读改删数据

--- koa2-learn/dbs/models/person.js
+++ koa2-learn/dbs/models/person.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 
 // 声明一个schema---描述表中的字段
+// 在 Mongoose 中，所有东西都从一个 Schema 开始。
 let personSchema = new mongoose.Schema({
     name: String,
     age: Number
 })
 
 // 由schema生成一个model并导出
+// 在 Mongoose 中，模型是通过已定义的 Schema 生成
 module.exports = mongoose.model('Person', personSchema)
 
 /**
@@ -16,4 +18,8 @@ module.exports = mongoose.model('Person', personSchema)
  *
  *   注意数据表的名字，默认为文件名---person.js
  *   所以models目录下的，文件名不要随便取，因为models目录下的文件名就是对象数据库里面的表的名字
+ *
+ *   mongoose: 集合collection----文档document----field
+ *
+ *   mysql: 表table----row ----column
 * */
--- koa2-learn/routes/users.js
+++ koa2-learn/routes/users.js
@@ -11,9 +11,10 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+// 1. 向数据库“增加”数据 --->实例.save()
 // 定义一个接口: addPerson，接口类型为post，需要给接口传name和age字段
 router.post('/addPerson', async function (ctx) {
-    //根据模型model创建实例
+    //根据模型model创建实例----文档
     const person = new Person({
         name: ctx.request.body.name,
         age:ctx.request.body.age
@@ -21,7 +22,8 @@ router.post('/addPerson', async function (ctx) {
     let code
     try {
         await person.save()
-        // 通过 save()方法，增添数据
+        // save()---实例方法
+        // 通过 实例.save()方法，增添数据
         // 这个save()方法，是model中已经定义好的，可以直接用
         code = 0
     }catch (e) {
@@ -31,6 +33,90 @@ router.post('/addPerson', async function (ctx) {
     ctx.body = {
       code: code
     }
-
 })
+
+// addPerson接口的访问方式：使用git bash，输入：
+// curl -d 'name=lilei&age=27' http://localhost:3000/users/addPerson
+// 返回结果为 { 'code': 0 }
+// curl 是 shell 命令，所以必须使用 git bash
+
+
+// 2. 从数据库“读取”数据---->find()、findOne()是模型的静态方法
+router.post('/getPerson',async function (ctx) {
+
+    // findOne()------>只是查找某一条记录，查到第一条即停止，不会继续向下查找
+    const result = await Person.findOne({
+        name: ctx.request.body.name
+    })
+
+    //find()------->返回符合条件的数据的集合
+    const results = await Person.find({
+        name: ctx.request.body.name
+    })
+    ctx.body = {
+        code:0,
+        result,
+        results
+    }
+})
+
+// getPerson的访问方式：使用git bash 输入：
+// curl -d 'name=lilei' http://localhost:3000/users/getPerson
+// 返回结果为：
+// {
+//     "code": 0,
+//     "result": {
+//     "_id": "5c7cd28a77df23331c3e9d7b",
+//         "name": "hanmeimei",
+//         "age": 17,
+//         "__v": 0
+// },
+//     "results": [
+//     {
+//         "_id": "5c7cd28a77df23331c3e9d7b",
+//         "name": "hanmeimei",
+//         "age": 17,
+//         "__v": 0
+//     },
+//     {
+//         "_id": "5c7cd6bd7fed7839c00ff43d",
+//         "name": "hanmeimei",
+//         "age": 38,
+//         "__v": 0
+//     }
+// ]
+// }
+
+// 3.改数据-----update()模型的静态方法
+router.post('/updatePerson', async function (ctx) {
+    const result = await Person.where({
+        name: ctx.request.body.name
+    }).update({
+        age:ctx.request.body.age
+    })
+    ctx.body = {
+        code: 0
+    }
+})
+// curl -d 'name=lilei&age=11' http://localhost:3000/users/updatePerson
+//{
+//   "code": 0
+// }
+// 数据库中的leilei这条记录age改为了11
+
+// 4. 删除数据-----remove()模型方法
+router.post('/removePerson', async function (ctx) {
+    const result = await Person.where({
+        name: ctx.request.body.name
+    }).remove()
+    ctx.body = {
+        code: 0
+    }
+})
+//curl -d 'name=lilei' http://localhost:3000/users/removePerson
+//{
+//   "code": 0
+// }
+// 数据库中的leilei这条记录被删除了
+
 module.exports = router
